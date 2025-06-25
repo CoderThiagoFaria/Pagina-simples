41de79e1de5e72d93bd4eaeffe45a36f92e78422
Thiago Faria <112523814+CoderThiagoFaria@users.noreply.github.com>
2025-06-25 09:55:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementsByClassName('mensagem-btn')[0].addEventListener('click', function() {
+    document.getElementsByClassName('mensagem')[0].textContent = 'Você é capaz de conquistar tudo o que deseja. Acredite em você!';
+});
